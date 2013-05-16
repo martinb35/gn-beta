f1acc2a7b43f035ce6f8db3e51c09c551b84e09b
Carlos Vences <dev@semantic.mx>
2013-05-16 15:15:04 -0500

    CV, init project for ruby 2, rails 4 and pg

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GoninisB1::Application.config.secret_key_base = '4f2902bb0dd936fe4c12e44ba42f7ef666c9c1ed2b7430e300b1f318db166a7cef9b08c040c0cf8e1133828d7f8d627060097ffdca33926a84296e922ef60661'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+GoninisB1::Application.config.session_store :cookie_store, key: '_goninis_b1_session'
