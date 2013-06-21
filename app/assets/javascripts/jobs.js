0f0a9eb6d3136db658608fad2d09ecb7338b0fd8
Carlos Vences <dev@semantic.mx>
2013-06-20 19:16:18 -0500

    CV, fix turbolinks - jQuery issue

--- app/assets/javascripts/jobs.js
+++ app/assets/javascripts/jobs.js
@@ -1,5 +1,12 @@
-jQuery (document).on('page:change', function() {
+function onInit_Jobs() {
   jQuery('section#listing > div.item').bind('click', function(item) {
     jQuery(this).next().toggle();
   });
-});
+}
+jQuery (document)
+  .on('page:change', function() {
+    onInit_Jobs();
+  })
+  .ready(function() {
+    onInit_Jobs();
+  });
