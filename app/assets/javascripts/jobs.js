1b0932b66ecbf3607dbf9f6b5ce6c126edc5f214
Carlos Vences <dev@semantic.mx>
2013-06-20 19:03:32 -0500

    CV, bundle install

--- app/assets/javascripts/jobs.js
+++ app/assets/javascripts/jobs.js
@@ -1,6 +1,5 @@
 jQuery (document).ready(function() {
   jQuery('section#listing > div.item').bind('click', function(item) {
     jQuery(this).next().toggle();
-    console.log ('Clicked! ' + item);
   });
 });
