00d18f10a3875f1ec9c1c6e83cf628d3426577c1
Carlos Vences <dev@semantic.mx>
2013-06-25 22:52:56 -0500

    CV, Implement logout button

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,6 +14,6 @@ class SessionsController < ApplicationController
 
   def destroy
     session[:user_id] = nil
-    redirect_to store_url, notice: "Logged out"
+    redirect_to tasks_url
   end
 end
