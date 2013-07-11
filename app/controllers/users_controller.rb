93a913f7e21af6b93778e4be1fa1bd95737456af
Carlos Vences <dev@semantic.mx>
2013-07-11 13:26:10 -0500

    CV, fix mailer

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,7 +29,7 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
-        format.html { redirect_to tasks_url }
+        format.html { redirect_to login_path }
         format.json { render action: 'show', status: :created, location: @user }
       else
         format.html { render action: 'new' }
