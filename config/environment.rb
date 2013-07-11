f50619bc901ea7c2ecadceb1a9361cecc4cac49a
Carlos Vences <dev@semantic.mx>
2013-07-11 16:28:49 -0500

    CV, test sendgrid config

--- config/environment.rb
+++ config/environment.rb
@@ -6,10 +6,11 @@ GoninisB1::Application.initialize!
 
 # Use GMAIL as external SMTP
 
+ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
   :user_name => ENV['SENDGRID_USERNAME'],
   :password => ENV['SENDGRID_PASSWORD'],
-  :domain => 'heroku.com',
+  :domain => 'goninis.com',
   :address => 'smtp.sendgrid.net',
   :port => 587,
   :authentication => :plain,
