9bbe2332c25e7110cb269fbec57ff3ab98ccbf9e
Carlos Vences <dev@semantic.mx>
2013-07-16 14:51:03 -0500

    CV, fix email links

--- app/mailers/job_mailer.rb
+++ app/mailers/job_mailer.rb
@@ -3,24 +3,25 @@ class JobMailer < ActionMailer::Base
   
   def notify_assigned(job, user_id)
     @job = job
+    @user_id = user_id
     @last_index = @job.contact.to_s.index('@')
-    @cat_name = JobCategory.find_by_id (@job.job_category).title
-    @url  = "http://goninis.com/jobs?id=#{@job.id}&status=3&revision=#{@user_id}"
+    @cat_name = JobCategory.find_by_id (@job.job_category_id).title
+    @url  = "http://pre.goninis.com/jobs?id=#{@job.id}&status=3&revision=#{@user_id}"
     mail(to: @job.contact, subject: 'goninis - Permiso para realizar tarea')
   end
   def notify_accepted(user, job)
     @job = job
     @user = user
-    @cat_name = JobCategory.find_by_id (@job.job_category).title
-    @url  = "http://goninis.com/jobs?id=#{@job.id}&status=4&revision=#{@user_id}"
+    @cat_name = JobCategory.find_by_id (@job.job_category_id).title
+    @url  = "http://pre.goninis.com/jobs?id=#{@job.id}&status=4&revision=#{@user.id}"
     mail(to: @user.email, subject: 'goninis - Felicidades, te han asignado una tarea')
   end
   def notify_rejected(user, job)
     @job = job
     @user = user
     @last_index = @job.contact.to_s.index('@')
-    @cat_name = JobCategory.find_by_id (@job.job_category).title
-    @url  = "http://goninis.com/jobs?id=#{@job.id}&status=3&revision=#{@user_id}"
+    @cat_name = JobCategory.find_by_id (@job.job_category_id).title
+    @url  = "http://pre.goninis.com/jobs?id=#{@job.id}&status=3&revision=#{@user.id}"
     mail(to: @job.contact, subject: 'goninis - Permiso para realizar tarea')
   end
 end
