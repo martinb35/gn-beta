43ac2805ab3b85c14d64d8d5813093986cc84e2a
gonzalojpv <gonzalojpv@openmailbox.org>
2014-02-12 22:49:17 -0600

    GP, task#21 pantalla de Gracias

--- app/assets/javascripts/thank.js
+++ app/assets/javascripts/thank.js
@@ -1,2 +1,26 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+// Place all the behaviors and hooks related to the matching controller here.
+// All this logic will automatically be available in application.js.
+
+var gn_thank = {
+
+  el:$('body.thank'),
+
+  initialize: function(){
+  	this.el = $('body.thank');
+    this.event();
+  },
+
+  event: function(){
+    var self = this;
+
+    $(window).resize(function(){
+      self.el.find('.container-body').height( $(this).height() - self.el.find('footer').height() - self.el.find('#brand').height() - 80 + 'px' );
+    }).resize();
+  }
+}
+
+$(function(){
+  gn_thank.initialize();
+});
